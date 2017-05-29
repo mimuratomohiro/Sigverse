cc875ba6a87301443c609624cc2f6a0feecc6a19
Tomohiro Mimura <kawasaki@TomohironoMacBook-Pro.local>
2017-05-29 15:50:32 +0900

    first commit

---
+++ Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    GameObject base_footprint_obj;
+    // Use this for initialization
+    void Start () {
+        base_footprint_obj = this.transform.FindChild("0.!joint_Master").gameObject;
+        print(base_footprint_obj);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //this.transform.rotation.SetAxisAngle();
+        //this.transform.FindChild().gameObject;
+        this.transform.Rotate(new Vector3(5, 5, 5));
+    }
+}
